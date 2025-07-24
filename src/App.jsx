ca659b514db0887f608f9547d2c0522a5da88cbb
Sara <sarailic431@gmail.com>
2025-07-24 23:15:08 +0200

    . . . . .. .. .. .. .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Form from "./Form";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Form />
+    </>
+  );
 }
 
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
-  return <form></form>;
+  const [newItemName, setNewItemName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newItemName);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Grocery Bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          Add Item
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default Form;
